72ece5ca6378f240f1727fc502ac89a6c91847fe
EdsonOli <edso06@gmail.com>
2021-03-25 10:55:08 -0300

    mudando um pouco

---
+++ index.js
@@ -0,0 +1,46 @@
+
+let nomePetshop = "PETSHOP AVANADE";
+
+let pets =[
+    {
+        nome: 'luna',
+        tipo: 'cachorro',
+        idade: 2,
+        raca: 'Vira-lata',
+        peso: 10,
+        tutor: 'Edson',
+        vacinado: true,
+        servicos: ['banho', 'vermifugação']
+    },
+    {
+        nome: 'foguete',
+        tipo: 'cachorro',
+        idade: 1,
+        raca: 'São bernardo',
+        peso: 30,
+        tutor: 'Luana',
+        vacinado: true,
+        servicos: ['banho', 'tosa']
+    },
+    {
+        nome: 'Bob',
+        tipo: 'gato',
+        idade: 3,
+        raca: 'cianes',
+        peso: 2,
+        tutor: 'barbara',
+        vacinado: false,
+        servicos: ['banho', 'vacinação']
+    },
+];
+
+// 
+
+const listarPets = () => {
+   
+    for(let i = 0; i < pets.length; i++){
+        console.log(pets[i].nome)
+    }
+}
+
+listarPets();
